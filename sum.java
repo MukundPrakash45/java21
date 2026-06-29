403fd993f812f1c7e84a7db3c1ecd4f5471f9d09
MukundPrakash45 <mukund2023@gift.edu.in>
2026-06-29 20:22:38 +0530

    day_1

---
+++ sum.java
@@ -0,0 +1,12 @@
+public class sum {
+   public sum() {
+   }
+
+   public static void main(String[] var0) {
+      int a = 10;
+      int b = 22;
+      int c = a + b;
+      System.out.println("Sum of two numbers: " + c);
+   }
+    
+}
