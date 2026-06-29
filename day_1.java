9f80addab701b96bedf0b96d1c7169c0e53dd73d
MukundPrakash45 <mukund2023@gift.edu.in>
2026-06-29 20:18:00 +0530

    Hello World

---
+++ day_1.java
@@ -0,0 +1,9 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class day_1 {
+   public day_1() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("Hello World");
+   }
+}
